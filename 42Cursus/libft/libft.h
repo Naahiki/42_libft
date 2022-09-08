0fbbd309607b49e6b62353ec5efa6e0c75d734ee
Nahikari Rodriguez Nogales <nrodrigu@c2r3s3.42urduliz.com>
2022-09-08 17:50:08 +0200

    Libft ona

--- 42Cursus/libft/libft.h
+++ 42Cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nrodrigu <nrodrigu@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 11:56:37 by nrodrigu          #+#    #+#             */
-/*   Updated: 2022/09/04 12:24:54 by nrodrigu         ###   ########.fr       */
+/*   Updated: 2022/09/07 17:15:26 by nrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,31 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stddef.h>
 
-// 1.Zatia - Funciones Standard C Library (libc, -lc)
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
-int				ft_isprint(int c);
-size_t			ft_strlen(const char *s);
-void			*ft_memset(void *b, int c, size_t len);
-void			ft_bzero(void *s, size_t n);
-void			*ft_memcpy(void *dst, const void *src, size_t n);
-void 			*memmove(void *dst, const void *src, size_t len);
-size_t			strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
-size_t			strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
-int				toupper(int c);
-int				tolower(int c);
-char 			*strchr(const char *s, int c);
-char 			*strrchr(const char *s, int c);
-int				strncmp(const char *s1, const char *s2, size_t n);
-int				memcmp(const void *s1, const void *s2, size_t n);
-char 			*strnstr(const char *haystack, const char *needle, size_t len);
-int				ft_atoi(const char *str);
+//Lehenengo Zatia - Funciones Standard C Library (libc, -lc)
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+size_t		ft_strlen(const char *s);
+void		*ft_memset(void *b, int c, size_t len);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int			ft_atoi(const char *str);
 
 #endif
