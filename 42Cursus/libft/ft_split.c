f3dfedba2f203d684904f57c95ffcda9500806c9
Nahikari Rodriguez Nogales <nrodrigu@c2r3s3.42urduliz.com>
2022-09-20 19:37:46 +0200

    20/09/22

--- 42Cursus/libft/ft_split.c
+++ 42Cursus/libft/ft_split.c
@@ -3,17 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nrodrigu <nrodrigu@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 12:17:15 by nrodrigu          #+#    #+#             */
-/*   Updated: 2022/09/19 12:56:21 by nrodrigu         ###   ########.fr       */
+/*   Updated: 2022/09/20 18:20:46 by nrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Reserva (utilizando malloc(3)) un array de strings resultante de separar la
  *  string s en substrings utilizando el caracter c como delimitador.
  *   El array debe terminar con un puntero NULL.*/
+/* PARAMETROS:*/
+/* char const *s, es la cadena que va a delimitar cada subcadena.*/
+/*  char c, es el caracter que va a delimitar cada subcadena*/
+/* Si falla reserva memoria--NULL*/
+/*Si c no aparece en s --> Devuelve la cadena s en la posicion [0] e la matriz*/
+/* Si c aparece en s -->Devuelve una matriz de los segmentos que haya.*/
 
 #include "libft.h"
 
-
+char	**ft_split(char const *s, char c)
+{
+	
+}
