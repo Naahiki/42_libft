a82d187b56d64e0615378ec80b386da57dce937b
Nahikari Rodriguez Nogales <nrodrigu@c2r3s3.42urduliz.com>
2022-09-23 19:15:47 +0200

    BonusPart_23/09

--- 42Cursus/libft/libft.h
+++ 42Cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nrodrigu <nrodrigu@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 11:56:37 by nrodrigu          #+#    #+#             */
-/*   Updated: 2022/09/19 12:22:49 by nrodrigu         ###   ########.fr       */
+/*   Updated: 2022/09/23 19:11:10 by nrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,14 @@
 # include <string.h>
 # include <stddef.h>
 
-//Lehenengo Zatia - Funciones Standard C Library (libc, -lc)
+/* *********Estructura necesaria para representar un nodo de tu lista********* */
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;	
+}					t_list;
+
+/* *********Lehenengo Zatia - Funciones Standard C Library (libc, -lc)********* */
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
 int			ft_isalnum(int c);
@@ -43,7 +50,7 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int			ft_atoi(const char *str);
 void		*ft_calloc(size_t count, size_t size);
 char		*ft_strdup(const char *s1);
-/* Funciones adicionales*/
+/* ************************Funciones adicionales********************************* */
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strtrim(char const *s1, char const *set);
@@ -55,4 +62,14 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+/* ************************PARTE BONUS - LISTAS ********************************* */
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
